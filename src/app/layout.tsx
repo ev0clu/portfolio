3b67c1d76a4fe1ca4bded155e2224705548e7d74
ev0clu <ev0clu@gmail.com>
2024-08-23 10:23:51 +0200

    Metadata fields are updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { siteConfig } from "@/config/site";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -9,8 +10,63 @@ const fontSans = FontSans({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Personal portfolio",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Javascript",
+    "Typescript",
+    "Tailwind CSS",
+    "Shadcn UI",
+    "Node.js",
+    "Express.js",
+    "Nest.js",
+    "MongoDB",
+    "PostgreSQL",
+    "Chart",
+    "Fullstack",
+    "Full stack",
+    "Frontend",
+    "Web developer",
+  ],
+  authors: [
+    {
+      name: "Laszlo Kis",
+      url: siteConfig.url,
+    },
+  ],
+  creator: "Laszlo Kis",
+  openGraph: {
+    type: "website",
+    locale: "hu_HU",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
+
+export const viewport: Viewport = {
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "black" }],
 };
 
 export default function RootLayout({
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Laszlo Kis Portfolio",
+  url: "https://lk.com",
+  ogImage: "https://lk.com/og.jpg",
+  description: "Laszlo Kis full stack developer portfolio website",
+  links: {
+    linkedin: "https://www.linkedin.com/in/laszlo-kis-1932a1276",
+    github: "https://github.com/ev0clu",
+  },
+};
+
+export type TSiteConfig = typeof siteConfig;
