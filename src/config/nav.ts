290b8407ac0851938239f49cd62b03a9b0418da3
ev0clu <ev0clu@gmail.com>
2024-08-28 14:02:57 +0200

    Navigation config is extended with type

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,1 +1,3 @@
-export const navConfig = ["Home", "About", "Projects", "Contact"];
+export const navConfig = ["Home", "About", "Projects", "Contact"] as const;
+
+export type TNavConfig = (typeof navConfig)[number];
