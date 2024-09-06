bd09270c6702ff059145893e8885e32f4b425bfd
ev0clu <ev0clu@gmail.com>
2024-09-06 11:55:35 +0200

    Motion is added to Contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -21,6 +20,7 @@ import {
   TContactFormSchema,
 } from "@/lib/validations/contactFormSchema";
 import Section from "../Section/Section";
+import SectionMotion from "../Section/SectionMotion";
 import SectionTitle from "../Section/SectionTitle";
 import { LoaderCircle } from "lucide-react";
 import { getReCaptchaToken } from "@/lib/recaptcha";
@@ -75,80 +75,82 @@ const Contact = () => {
 
   return (
     <Section id="Contact" className="pb-[7.5rem]">
-      <SectionTitle>Contact</SectionTitle>
-      <p className="pb-8 text-center text-muted-foreground">
-        Feel free to Contact me by submitting the form below and I will get back
-        to you as soon as possible
-      </p>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="min-w-full space-y-4 md:min-w-[70%] lg:min-w-[50%]"
-        >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="John Doe" type="text" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="example@mail.com"
-                    type="email"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="message"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Message</FormLabel>
-                <FormControl>
-                  <Textarea
-                    {...field}
-                    placeholder="Type your message here."
-                    className="resize-none"
-                    rows={10}
-                    disabled={false}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="text-center">
-            <Button type="submit" disabled={isSubmitting}>
-              Submit
-              {isSubmitting && (
-                <LoaderCircle
-                  strokeWidth={2.5}
-                  className="ml-2 h-4 w-4 animate-spin text-emerald-500"
-                />
+      <SectionMotion>
+        <SectionTitle>Contact</SectionTitle>
+        <p className="pb-8 text-center text-muted-foreground">
+          Feel free to Contact me by submitting the form below and I will get
+          back to you as soon as possible
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="min-w-full space-y-4 md:min-w-[70%] lg:min-w-[50%]"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="John Doe" type="text" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-            </Button>
-          </div>
-        </form>
-      </Form>
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="example@mail.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Message</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      placeholder="Type your message here."
+                      className="resize-none"
+                      rows={10}
+                      disabled={false}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="text-center">
+              <Button type="submit" disabled={isSubmitting}>
+                Submit
+                {isSubmitting && (
+                  <LoaderCircle
+                    strokeWidth={2.5}
+                    className="ml-2 h-4 w-4 animate-spin text-emerald-500"
+                  />
+                )}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </SectionMotion>
     </Section>
   );
 };
